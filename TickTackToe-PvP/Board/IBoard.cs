f04491c0ff84632ae45fd112a123700c7f931a4d
Stan Nedelkov <stenly.nedelkov@gmail.com>
2022-09-24 20:29:51 +0300

    You can now draw the board.

--- TickTackToe-PvP/Board/IBoard.cs
+++ TickTackToe-PvP/Board/IBoard.cs
@@ -6,6 +6,10 @@ namespace TickTackToe_PvP.Board
 {
     public interface IBoard
     {
-        public string[,] DrawBoard(int row,int col, bool playerTurn);
+        public string DrawBoard(int row,int col, bool player01Turn, bool player02Turn);
+
+        public string InitialBoard();
+
+        public string [,] BoardMatrix { get; }
     }
 }
